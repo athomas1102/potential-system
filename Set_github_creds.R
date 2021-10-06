16542155f727cc1980583f71043338c0ef6f62a1
Amy Thomas <thomasfamily@amys-air.win.ad.jhu.edu>
2021-10-06 13:04:15 -0400

    wrote comments for calculations

--- Set_github_creds.R
+++ Set_github_creds.R
@@ -15,3 +15,19 @@ usethis::create_github_token()
 gitcreds::gitcreds_set()
 # checks PAT stored correctly
 gitcreds::gitcreds_get()
+# created a vector to show numbers 0 - 15 by using <-
+# to comment use command shift c
+v <- c(0:15)
+# tells me how many numbers are in vector v
+length(v)
+# created a second vector b to show number 30 - 45 by using <-
+b <- c(30:45)
+# tells me how many numbers are in vector b
+length(b)
+
+# created a vector to add the 16 numbers in v to b
+sum <- v + b
+# tells me how many numbers result when adding v + b
+length(sum)
+sum
+# don't forget to save the file
